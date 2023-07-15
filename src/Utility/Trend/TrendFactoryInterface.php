e5e11ef18f0373bc2000813da00087440b79df46
Jens Scherbl <jens@scherbl.com>
2023-07-15 17:35:12 +0200

    Removes obsolete utility structures

--- src/Utility/Trend/TrendFactoryInterface.php
+++
@@ -1,8 +0,0 @@
-<?php declare(strict_types=1);
-
-namespace Kensho\Indicators\Utility\Trend;
-
-interface TrendFactoryInterface
-{
-    public static function create(int $SMAPeriod, int $EMAPeriod): TrendInterface;
-}
